35426c329f9aaa5bd729ba0f1c2809de88cdca71
masterscode <ogbinakaemmanuel@gmail.com>
2020-03-24 15:38:13 +0100

    still on it though not clear still

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,7 +1,11 @@
 <?php
 
 class Home extends Controller{
-    public function index(){
-        echo 'home/index';
+    public function index($param = '', $paramTwo = ''){
+        echo 'no value passed in';
+        echo $param . $paramTwo;
+    }
+    public function login(){
+        print 'login here';
     }
 }
--- app/core/App.php
+++ app/core/App.php
@@ -7,20 +7,33 @@ class App{
      
      public function __construct(){
         $url = $this->parseUrl();
-
+        // print_r($url);
         if(file_exists("../app/controllers/$url[0].php")) {
             $this->controller = $url[0];
             unset($url[0]);
         }
 
         require "../app/controllers/$this->controller.php";
-        print $this->controller;
+         $this->controller = new $this->controller();
+        //  print_r($this->controller);
+        if ( isset($url[1]) && method_exists($this->controller, $url[1])){
+            // echo 'ok';
+            $this->method = $url[1];
+            unset($url[1]);
+            
+        }
+
+        $this->params = $url ? array_values($url) : [];
+        // print $this->params;
+        call_user_func_array([$this->controller, $this->method], $this->params); 
      }
 
      public function parseUrl(){
          if(isset($_GET['url'])){
             //  var_dump( parse_url($_GET['url']));
-            $sanitizedUrl = filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL);
+            $sanitizedUrl = filter_var(
+                rtrim($_GET['url'], '/'), 
+                FILTER_SANITIZE_URL);
             return explode('/',$sanitizedUrl);
              
          }
